b855ad2bd6581848c3db8ef70f3f54c0526779f2
Georgia Jefferson <82558216+GeorgiaAgileWebDev@users.noreply.github.com>
2023-04-20 19:21:46 +0800

    Finished Setup
    All base libraries have been imported. Run the print statements just to check that you've installed everything properly.
    
    Updated README.md with some research for the basis of our algorithm.
    
    Updated reguirements.txt with added libraries.
    
    All work from here should be done on separate branches.

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 pip3 install -r requirements.txt
 ```
 2.  Lorem ipsum dolor sit amet, consectet
+
+## Research
+Some articles that might be useful:
+- "An Automated Trading System with Multi-indicator Fusion Based on GA and SVM" by J. Liu, L. Liu, and Z. Gao (2018)
+- "Using Evolutionary Algorithms to Build Technical Trading Rules for Indian Stock Markets" by R. Sharma and P. Kumar (2014)
+- "Optimizing Trading Strategies through an Agent-Based Evolutionary Approach" by M. Ormos, P. Korosi, and G. Varga (2013)
+- "A Genetic Algorithm Approach to Trading System Development" by J. Weiming and L. Xiaojun (2009)
--- bot.py
+++ bot.py
@@ -1,9 +1,33 @@
 import ccxt
 import ta
+import pandas as pd
+#Bollinger Bands help identify sharp, short-term price movements and potential entry and exit points.
+#ATR measures volatility, taking into account any gaps in the price movement. Typically, ATR calculation is based on 14 periods - can be intraday, daily, weekly, or monthly.
+from ta.volatility import BollingerBands, AverageTrueRange 
 
+#Sets up data & variables
 exchange = ccxt.kraken()
 bitcoin_data = exchange.fetch_ohlcv('BTC/AUD', timeframe='1d', limit=270)
+df = pd.DataFrame(bitcoin_data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+bb_indicator = BollingerBands(df['close'])
 
 #Just testing it works
 #for candle in bitcoin_data:
     #print(candle)
+
+
+#Adds upper band to dataframe
+df['upper_band'] = bb_indicator.bollinger_hband()
+
+#Adds lower band to dataframe 
+df['lower_band'] = bb_indicator.bollinger_lband()
+
+#Adds moving average to dataframe
+df['moving_average']=bb_indicator.bollinger_mavg()
+
+#Adds average true range indicator to dataframe
+atr_indicator = AverageTrueRange(df['high'], df['low'], df['close'])
+df['atr'] = atr_indicator.average_true_range()
+
+#Just testing it works
+#print(df)
