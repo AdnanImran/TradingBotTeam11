224ea7d11fe26120b316d0ebce9a616542be2eb4
Georgia Jefferson <82558216+GeorgiaAgileWebDev@users.noreply.github.com>
2023-04-29 11:34:49 +0800

    Created method for  optimize and fitness
    -Created method for our optimization and fitness function. There's nothing in there yet. Just gives us a workspace moving forward.
    -Added comments about optimization parameters we need to consider.

--- bot.py
+++ bot.py
@@ -2,20 +2,14 @@ import ccxt
 import ta
 import pandas as pd
 #Bollinger Bands help identify sharp, short-term price movements and potential entry and exit points.
-#ATR measures volatility, taking into account any gaps in the price movement. Typically, ATR calculation is based on 14 periods - can be intraday, daily, weekly, or monthly.
 from ta.volatility import BollingerBands, AverageTrueRange
 from ta.trend import SMAIndicator
 
 #Sets up data & variables
 exchange = ccxt.kraken()
-bitcoin_data = exchange.fetch_ohlcv('BTC/AUD', timeframe='1d', limit=270)
+bitcoin_data = exchange.fetch_ohlcv('BTC/AUD', timeframe='1d', limit=720)
 df = pd.DataFrame(bitcoin_data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
 bb_indicator = BollingerBands(df['close'],window=5)
-
-#Just testing it works
-#for candle in bitcoin_data:
-    #print(candle)
-
 
 #Adds upper band to dataframe
 df['upper_band'] = bb_indicator.bollinger_hband()
@@ -33,8 +27,24 @@ df['atr'] = atr_indicator.average_true_range()
 #Adds Simple Moving Average to the dataframe
 df['SMA'] = SMAIndicator(df['close'],window=5).sma_indicator()
 
-#Just testing it works
-#print(df.rows)
+#Optimization Algorithm
+def optimize():
+    """Typical parameters used in DE are listed as following:
+    D – Problem dimension -> Defined by the number of decision variables or parameters that are being optimized in the fitness function. 2 (or 3 - will we include fillna?)
+    N – Population size (pop_var) -> How many input variables will be consider each time?
+    Cr – Crossover probability (cross_prob) -> value between 0 and 1. 0 means there will be no crossover. 
+    #The higher the value the more the algorithm will explore. The lower the value the more the algorithm can refine promising solution spaces. 
+    #Can we do a high value first and then create a list of optimal solutions and run it once more and lower the value?
+    F – Scaling factor ->  Controls the amplification of the difference vector (difference between two randomly selected individuals from the population) used in the mutation step. 
+    G – Number of generation/stopping condition -> Decide how many iterations should be considered.
+    Li,Hi – boundary for dimension i -> the range of values that 'dimension i' of each candidate solution is allowed to take during the optimization process. These boundaries help to constrain the search space of the algorithm.
+    #Basically it helps ensure that candidate solutions remain within the feasible region of the search space.
+    """
+    return None
+
+#Tells the DE algorithm if a solution is viable or not.
+def fitness(candidate_solution, df):
+    return None
 
 
 #Buy Trigger to return true or false if we should buy on a particular timestamp according to our parameters
@@ -55,3 +65,5 @@ def sell(timestamp, parameters):
     if timestamp == 1:
         return False
     return True
+
+
