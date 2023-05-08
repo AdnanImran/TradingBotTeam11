f73e7263267aa6b64a4aad5b5180d6abef4b281a
timpye <72972212+timpye@users.noreply.github.com>
2023-05-08 21:28:21 +0800

    Added limit to days spent idle
    You can now pass a buyLimit parameter to the trade function determining how long it can spend not doing anything before dorcing it to buy or sell

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import ta
 import pandas as pd
 import random
 import math
+import numpy
 #Bollinger Bands help identify sharp, short-term price movements and potential entry and exit points.
 from ta.volatility import BollingerBands, AverageTrueRange
 from ta.trend import SMAIndicator, EMAIndicator
@@ -39,11 +40,11 @@ df['simplified_timestamp'] = pd.to_numeric((df['timestamp'] - df['timestamp'].lo
 df['simplified_timestamp'] = pd.to_numeric((df['timestamp'] - df['timestamp'].loc[0])  / (df['timestamp'].loc[2] - df['timestamp'].loc[1]), downcast='signed')
 
 #Optimization Algorithm
-def optimize():
+def optimize(buyLimit):
     popSize = 10 #Population size -> How many versions of parameters will be created in each generation
     recombinationValue = 0.7
     mutationValue = 0.6 #Scaling factor ->  Controls the amplification of the difference vector (difference between two randomly selected individuals from the population) used in the mutation step.
-    gen = 50  #Number of generation/stopping condition -> Decide how many iterations should be considered.     
+    gen = 10  #Number of generation/stopping condition -> Decide how many iterations should be considered.     
     bounds = [(0,1),(1,720),(0,100)] #Li,Hi – boundary for dimension i -> These boundaries help to constrain the search space of the algorithm.
 
     # Initialise Population and randomly pick values for the parameters
@@ -115,8 +116,8 @@ def optimize():
             #--- SELECTION ----------
             # This is a greedy method where if the new score is greater than previous score it will take new one
             # In this example scenario this is to max the area of a circle
-            newScore  = trade(selected)
-            oldScore = trade(xT)
+            newScore  = trade(selected, buyLimit)
+            oldScore = trade(xT, buyLimit)
 
             if newScore > oldScore:
                 population[j] = selected
@@ -223,8 +224,9 @@ def sell(timestamp, df):
     return False
 
 # PARAMETERS = [ TRIGGER_INDICATOR (SMA=0, CLOSE=1, EMA=2), BOLLINGER_BANDS_WINDOW, TRIGGER_WINDOW_SIZE ] 
-def trade(parameters):
+def trade(parameters, buyLimit = 720):
     #initialise temp dataframe
+    print("Start Trade")
     eval_df = pd.DataFrame()
     eval_df["simplified_timestamp"] = df["simplified_timestamp"]
     BOLLINGER_BANDS_WINDOW = round(parameters[1])
@@ -234,18 +236,28 @@ def trade(parameters):
         eval_df["Indicator"] = SMAIndicator(df['close'],window=TRIGGER_WINDOW_SIZE).sma_indicator()
     elif round(parameters[0]) == 1: #CLOSE
         eval_df["Indicator"] = df['close']
-    #elif round(parameters[0]) == 2: #EMA
-        #eval_df["Indicator"] = EMAIndicator(df['close'], window = TRIGGER_WINDOW_SIZE).ema_indicator()
+    elif round(parameters[0]) == 2: #EMA
+        eval_df["Indicator"] = EMAIndicator(df['close'], window = TRIGGER_WINDOW_SIZE).ema_indicator()
     
     #BOLLINGER BANDS
     bands = BollingerBands(df['close'],window=BOLLINGER_BANDS_WINDOW)
     eval_df["upper"] = bands.bollinger_hband()
     eval_df["lower"] = bands.bollinger_lband()
-    
+    print( "Dataframe created")
+    counter = 0
     money = 100
     bitcoin = 0
     for row in range(len(df)):
-        if buyTrigger(row, eval_df):
+        if counter == buyLimit:
+            if money == 0:
+                money += bitcoin * df['close'].loc[row]
+                bitcoin = 0
+                counter = 0
+            else:
+                bitcoin += money / df['close'].loc[row]
+                money = 0
+                counter = 0
+        elif buyTrigger(row, eval_df):
             bitcoin += money / df['close'].loc[row]
             money = 0
         elif row == len(df)-1:
@@ -253,14 +265,16 @@ def trade(parameters):
         elif sellTrigger(row, eval_df):
             money += bitcoin * df['close'].loc[row]
             bitcoin = 0
+        counter += 1
         #print(row, "Money:", money, "Bitcoin:", bitcoin)
+    print("End Train")
     return money
 
 
 
 '''Three Key Functions'''
 # Generate optimal parameters.
-tradeParameters = optimize()
+tradeParameters = optimize(30)
 print(tradeParameters)
 # Run the trade.
 results = trade(tradeParameters)
