1e372fabbccfde9e7d62c076b4ccdec9b3670d40
AdnanImran <adnanimran1219@icloud.com>
2023-05-12 18:56:25 +0800

    Cleaning up code, removing old files and adding student numbers to readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# PomAndAussies [AIGroup11]
+# Trading Bot  [AIGroup11]
 
+Georgia Jefferson (23073856)
+
+Timothy Pye (22708266)
+
+Igor Pavkov (23193276)
+
+Thomas Whitehead (23749504)
+
+Adnan Imran (22514057)
 
 
 ## Setup
--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bitcoin_data = exchange.fetch_ohlcv('BTC/AUD', timeframe='1d', limit=720)
 
 # split ratio in range (0,1]. default = 0.8 (80% training, 20% testing). Set to 1 to test the whole 2 year period against etherium
 split_ratio = 1
-test_at_start = False  # Should test data be start or end of 2-year period?
+test_at_start = False 
 
 split = round(split_ratio*720)
 
@@ -76,30 +76,30 @@ def optimize(buyLimit, hyperparameters = [10,0.7,0.5,20]):
     population = initPopulation(popSize, bounds)
     
     # Initialise Lists for keeping track of results
-    best_solution = []
-    average_solution = []
+    bestSolution = []
+    averageSolution = []
      # Go through each generation
     for i in range(1,gen+1):
         print('GENERATION:',i)
 
         # Keep track of each generations score
-        gen_scores = [] 
+        genScores = [] 
         # Go through each individual in the population
         for j in range(0, popSize):
-            #--- MUTATION -----
-            # Select three random vector index positions [0, popSize), not including current vector (j)
+            # MUTATION STEP
+            # Select three random vector index positions, and not including current vector (j)
             # from the current generation that are both unique to themselves, but also unique to the 
             # currently selected individual that we're mutating (j)
             candidates = list(range(popSize))
             candidates.remove(j)
             # Random.sample method handles the unique selection
             # Parameters are the population and number of index positions to select
-            random_index = random.sample(candidates, 3)
+            randomIndices = random.sample(candidates, 3)
 
             # Get the values from the actual population
-            x1 = population[random_index[0]]
-            x2 = population[random_index[1]]
-            x3 = population[random_index[2]]
+            x1 = population[randomIndices[0]]
+            x2 = population[randomIndices[1]]
+            x3 = population[randomIndices[2]]
             xT = population[j]
 
             # NOTE: Mutation strategy taken from this article: 
@@ -117,18 +117,17 @@ def optimize(buyLimit, hyperparameters = [10,0.7,0.5,20]):
                 xDiff.append(diff)
 
             # multiply xDiff by the mutation factor (F) and add to x1
-            vectorDonor = []
+            mutatedParams = []
             for x1_i, xDiff_i in zip(x1, xDiff):
                 v = x1_i + mutationValue * xDiff_i
-                vectorDonor.append(v)
+                mutatedParams.append(v)
             
             # Checks that new values are within the bounds of our parameters
-            vectorDonor = checkBounds(vectorDonor, bounds)
+            mutatedParams = checkBounds(mutatedParams, bounds)
 
-            #--- RECOMBINATION/CROSSOVER ----------
+            # RECOMBINATION/CROSSOVER STEP
             # Recombination incorporates successful solutions from the previous generation
-            # Here we randomly select which parameters should continue on, since this example only
-            # has one parameter, it will randomly choose whether to take the parent or mutated child
+            # Here we randomly select which parameters should continue on
 
             selected = []
             # cycle through each variable in our target vector
@@ -137,40 +136,39 @@ def optimize(buyLimit, hyperparameters = [10,0.7,0.5,20]):
                 
                 # recombination occurs when crossover <= recombination rate
                 if crossover <= recombinationValue:
-                    selected.append(vectorDonor[k])
+                    selected.append(mutatedParams[k])
 
                 # recombination did not occur
                 else:
                     selected.append(xT[k])
-            #--- SELECTION ----------
+            # SELECTION STEP
             # This is a greedy method where if the new score is greater than previous score it will take new one
-            # In this example scenario this is to max the area of a circle
             newScore  = trade(selected, buyLimit)
             oldScore = trade(xT, buyLimit)
 
             if newScore > oldScore:
                 population[j] = selected
-                gen_scores.append(newScore)
+                genScores.append(newScore)
 
             else:
-                gen_scores.append(oldScore)
+                genScores.append(oldScore)
 
         # Print each generations stats
 
         # current generation avg. fitness
-        gen_avg = sum(gen_scores) / popSize
+        genAvg = sum(genScores) / popSize
         # fitness of best individual
-        gen_best = max(gen_scores)
+        genBest = max(genScores)
         # solution of best individual
-        gen_sol = population[gen_scores.index(max(gen_scores))]
+        genSol = population[genScores.index(max(genScores))]
 
-        print('> GENERATION AVERAGE:',gen_avg)
-        print('> GENERATION BEST:',gen_best)
-        print('> BEST SOLUTION:',gen_sol,'\n')
-        best_solution.append(gen_best)
-        average_solution.append(gen_avg)
-    print(gen_sol)
-    return (gen_sol, best_solution, average_solution)
+        print('> GENERATION AVERAGE:',genAvg)
+        print('> GENERATION BEST:',genBest)
+        print('> BEST SOLUTION:',genSol,'\n')
+        bestSolution.append(genBest)
+        averageSolution.append(genAvg)
+    print(genSol)
+    return (genSol, bestSolution, averageSolution)
 
 # Ensure that new values obtained through mutation are within the bounds of the parameters
 def checkBounds(solutions, bounds):
@@ -311,24 +309,16 @@ def trade(parameters, buyLimit = 720,verbose=False):
 
 '''Three Key Functions'''
 # Generate optimal parameters.
-#buyLimit = 350
+buyLimit = 30
 #tradeParameters = optimize(buyLimit)
 #print(numpy.round(tradeParameters))
+
 # Run the trade.
 #results = trade(tradeParameters, buyLimit)
+
 # Test performance of optimization.
 #successRate = evaluate(results, buyLimit)
 #print(f"Success Rate: {successRate}")
-
-#print(trade([1,20,2], buyLimit))
-# Report results.
-#print("Final value: $" & results)
-#if successRate > 0:
-    #print("Trading bot performed " & successRate & "\% better than the __chosen baseline__.")
-#elif successRate < 0:
-    #print("Trading bot performed " & abs(successRate) & "% poorer than the __chosen baseline__. Output was not successfully optimized.")
-#else:
-    #print("Trading bot's performance matched the __chosen baseline__. Output was not successfully optimized.")
 
 
 # FORWARD TESTING
